0a747825ef6b361c61798c4e4a2f26c2982b3236
ABoni14 <aboni150@gmail.com>
2021-11-23 14:37:05 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="ab-container">
+    <div class="ab-contain">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <Title />
+      <Main />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Title from "./components/Title.vue"
+import Main from "./components/Main.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Title,
+    Main
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.ab-container{
+  width: 100%;
+  height: 100vh;
+  background-color: aquamarine;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+}
+
+.ab-contain{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 600px;
+  height: 800px;
+  background-color: white;
+  border-radius: 10px;
+  border: 2px solid black;
+}
+
+img{
+  width: 300px;
 }
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>Devo dire che è stupendo!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  nome: "Main"
+}
+</script>
+
+<style scoped>
+  p{
+    font-size: 30px;
+    margin-top: 30px;
+    font-weight: bold;
+    text-decoration: underline;
+  }
+</style>
